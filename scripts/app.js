dca8866294e393eda8e854b128557ea23c8020af
KasiaW <kasia.aguti@gmail.com>
2019-06-03 11:39:00 +0100

    objects and array

--- scripts/app.js
+++ scripts/app.js
@@ -5,16 +5,51 @@ console.log('does it work?')
 const width = 10
 const squares = []
 let playerIndex = 0
-let activeTetramino = 'I'
-const tetrominos = ['I', 'O', 'T', 'J', 'L', 'S', 'Z']
+// let activeTetromino = 'I'
+// const tetrominos = ['I', 'O', 'T', 'J', 'L', 'S', 'Z']
+
+
+let tetromino = {
+  type: 'I',
+  position: [3, 4, 5, 6],
+  rotation: 0
+}
+// let tetromino = {
+//   type: 'J',
+//   position: [3, 13, 14, 15],
+//   rotation: 0
+// }
+// let tetromino = {
+//   type: 'L',
+//   position: [5, 13, 14, 15],
+//   rotation: 0
+// }
+// let tetromino = {
+//   type: 'T',
+//   position: [4, 13, 14, 15],
+//   rotation: 0
+// }
+// let tetromino = {
+//   type: 'S',
+//   position: [6, 5, 15, 14],
+//   rotation: 0
+// }
+// let tetromino = {
+//   type: 'Z',
+//   position: [4, 5, 15, 16],
+//   rotation: 0
+// }
+// let tetromino = {
+//   type: 'O',
+//   position: [4, 5, 14, 15],
+//   rotation: 0
+// }
 
 // //define tetraminos
-// const tetraminoNumber = Math.floor(Math.random() * 6)
+// const tetrominoNumber = Math.floor(Math.random() * 6)
 //
 // if (tetraminoNumber === 0) {
 //   activeTetramino = 'I'
-
-
 // }
 // else if (tetraminoNumber === 1){
 //   activeTetramino = 'O'
@@ -39,25 +74,26 @@ const grid = document.querySelector('.grid')
 
 function movePlayer() {
   squares.forEach(square => square.classList.remove('player'))
-//  squares[playerIndex].classList.add('player')
-  if (activeTetramino === 'I') {
-     squares[playerIndex].classList.add('player')
-     squares[playerIndex+1].classList.add('player')
-     squares[playerIndex+2].classList.add('player')
-     squares[playerIndex+3].classList.add('player')
+  squares[playerIndex].classList.add('player')
+  if (tetromino.type === 'I') {
+    tetromino.position.forEach(number => squares[number].classList.add('player'))
+    squares[playerIndex].classList.add('player')
+    squares[playerIndex+1].classList.add('player')
+    squares[playerIndex+2].classList.add('player')
+    squares[playerIndex+3].classList.add('player')
   }
 }
 function rotate(e) {
-switch(e.keyCode) {
-  case 77:
-  if (activeTetramino === 'I') {
-     squares.forEach(square => square.classList.remove('player'))
-     squares[playerIndex+2].classList.add('player')
-     squares[playerIndex+12].classList.add('player')
-     squares[playerIndex+22].classList.add('player')
-     squares[playerIndex+32].classList.add('player')
-}
-}
+  switch(e.keyCode) {
+    case 77:
+      if (tetromino.type === 'I') {
+        squares.forEach(square => square.classList.remove('player'))
+        squares[playerIndex+2].classList.add('player')
+        squares[playerIndex+12].classList.add('player')
+        squares[playerIndex+22].classList.add('player')
+        squares[playerIndex+32].classList.add('player')
+      }
+  }
 }
 
 
@@ -108,11 +144,11 @@ function init() {
     squares.push(square)
     grid.append(square)
   }
-     squares[playerIndex].classList.add('player')
-   }
+  squares[playerIndex].classList.add('player')
+}
 
-  window.addEventListener('keydown', handleKeyDown)
-  window.addEventListener('keydown', rotate)
+window.addEventListener('keydown', handleKeyDown)
+window.addEventListener('keydown', rotate)
 
 
 window.addEventListener('DOMContentLoaded', init)
