c240949566d254377c9a1d7c02d0bb93b273d012
KasiaW <kasia.aguti@gmail.com>
2019-06-04 14:25:38 +0100

    borders and rotation syntax

--- scripts/app.js
+++ scripts/app.js
@@ -5,69 +5,81 @@ console.log('does it work?')
 const height = 20
 const width = 10
 const squares = []
-const playerIndex = 0
-let activeTetromino =[]
+let divIndex = []
+let activeTetromino = {}
+
+
 
 const tetromino = [
-  {type: 'I', position: [3, 4, 5, 6]},
-  {type: 'J', position: [3, 13, 14, 15]},
-  {type: 'L', position: [5, 13, 14, 15]},
-  {type: 'T', position: [4, 13, 14, 15]},
-  {type: 'S', position: [6, 5, 15, 14]},
-  {type: 'Z', position: [4, 5, 15, 16]},
-  {type: 'O', position: [4, 5, 14, 15]}
+  // {type: 'I', position: [3, 4, 5, 6], orientation: 0},
+  {type: 'J', position: [13, 14, 15, 25],orientation: 0}
+  // {type: 'L', position: [24, 14, 15, 16], orientation: 0},
+  // {type: 'T', position: [4, 13, 14, 15], orientation: 0},
+  // {type: 'S', position: [6, 5, 15, 14], orientation: 0},
+  // {type: 'Z', position: [4, 5, 15, 16], orientation: 0},
+  // {type: 'O', position: [4, 5, 14, 15], orientation: 0}
 ]
+
+//
+// rotation
+// J 90 {-9, +0, +9, -2)
+//   180 {+2, +0, -2, -22}
+//   360 {+11, 0, -9, -19 }
+//
+// L 90 {-10, -9, +0, +9}
+//   180 {-18, +2, +0, -2}
+//   360 {+2, +11, +0, -9}
+//
+//
+
+function rotate() {
+  activeTetromino.orientation += 90
+  if (activeTetromino.orientation > 270) {
+    activeTetromino.orientation = 0
+  }
+  if (activeTetromino.type === 'J' && activeTetromino.orientation === 90) {
+    activeTetromino.position[0] = activeTetromino.position[0] - 9
+    activeTetromino.position[2] = activeTetromino.position[2] + 9
+    activeTetromino.position[3] = activeTetromino.position[3] - 2
+  }
+}
 
 
 const grid = document.querySelector('.grid')
 
 function generateTetromino (){
-  const tetrominoNumber = Math.floor(Math.random() * 7)
+  const tetrominoNumber = Math.floor(Math.random() * tetromino.length)
   return tetromino[tetrominoNumber]
+  // activeTetromino.positions.forEach(position => divIndex.push(position))
 }
-
 
 function movePlayer() {
   console.log(activeTetromino)
   // console.log('happening')
   squares.forEach(square => square.classList.remove('player'))
   activeTetromino.position.forEach(number => squares[number].classList.add('player'))
-  // if (tetromino.type) {
-  //   // tetromino.position = tetromino.position.map(pos => pos + 10)
-  //   tetromino.position.forEach(number => squares[number].classList.add('player'))
-  //
-  //
-  //   // if (currentShapeHorizontal) {
-  //   //   squares[playerIndex].classList.add('player')
-  //   //   squares[playerIndex+1].classList.add('player')
-  //   //   squares[playerIndex+2].classList.add('player')
-  //   //   squares[playerIndex+3].classList.add('player')
-  //   // } else {
-  //   //   squares[playerIndex].classList.add('player')
-  //   //   squares[playerIndex+width*1].classList.add('player')
-  //   //   squares[playerIndex+width*2].classList.add('player')
-  //   //   squares[playerIndex+width*3].classList.add('player')
-  //   // }
-  // }
 }
 
 function handleKeyDown(e) {
   let playerShouldMove = true
   switch(e.keyCode) {
     case 39:
-      if (activeTetromino) {
-        activeTetromino.position = activeTetromino.position.map(pos => pos + 1)
+      if (activeTetromino.position.every(pos => pos % width < width - 1)) {
+        activeTetromino.position = activeTetromino.position.map(x => x + 1)
       }
       break
     case 37:
-      if (activeTetromino) {
-        activeTetromino.position = activeTetromino.position.map(pos => pos - 1)
+      if (activeTetromino.position.every(pos => pos % width > 0)) {
+        activeTetromino.position = activeTetromino.position.map(x => x - 1)
       }
       break
     case 40:
-      if (activeTetromino) {
-        activeTetromino.position = activeTetromino.position.map(pos => pos + 10)
+      if (activeTetromino.position.every(pos => pos + 10 < 199)) {
+        activeTetromino.position = activeTetromino.position.map(x => x + 10)
       }
+      break
+    case 82:
+      rotate()
       break
     default:
       playerShouldMove = false
@@ -76,23 +88,35 @@ function handleKeyDown(e) {
 }
 
 
-function rotate(e) {
-	  switch(e.keyCode) {
-	    case 77:
-	      if (activeTetromino) {
-	        squares.forEach(square => square.classList.remove('player'))
-	        squares[playerIndex+2].classList.add('player')
-	        squares[playerIndex+12].classList.add('player')
-	        squares[playerIndex+22].classList.add('player')
-	        squares[playerIndex+32].classList.add('player')
-	        activeTetromino.position
-	        currentShapeHorizontal = false
-	      }
-	  }
-	}
+// function rotate(e) {
+// 	switch(e.keyCode) {
+// 	  case 38:
+// 	    if (activeTetromino) {
+//
+//
+//       }
+//       break
+// 	}
+//    movePlayer()
+// }
+  //   // if (currentShapeHorizontal) {
+  //   //   squares[playerIndex].classList.add('player')
+  //   //   squares[1].classList.add('player')
+  //   //   squares[2].classList.add('player')
+  //   //   squares[playerIndex+3].classList.add('player')
+  //   // } else {
+  //   //   squares[playerIndex].classList.add('player')
+  //   //   squares[playerIndex+width*1].classList.add('player')
+  //   //   squares[playerIndex+width*2].classList.add('player')
+  //   //   squares[playerIndex+width*3].classList.add('player')
+  //   // }
+  // }
+// }
+
 
 
 function init() {
+
   //  our code goes here
 
 
@@ -107,6 +131,7 @@ function init() {
     square.innerHTML = i
     squares.push(square)
     grid.append(square)
+    square.id = 'div' + i
   }
 
   activeTetromino = generateTetromino()
@@ -114,9 +139,15 @@ function init() {
   movePlayer()
 }
 
+//
+// function runGame(){
+// const interval = setInterval(tic, 200)
+// runGame()
+// }
 
 window.addEventListener('keydown', handleKeyDown)
-window.addEventListener('keydown', rotate)
+// window.addEventListener('keydown', rotate)
+
 
 
 window.addEventListener('DOMContentLoaded', init)
